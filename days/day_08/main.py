efdd900bcdeb1da23912c5aaa0593630fec56168
Matěj Račinský <racinsky.matej@seznam.cz>
2022-12-09 11:43:55 +0100

    day 8 part 2

--- days/day_08/main.py
+++ days/day_08/main.py
@@ -20,14 +20,27 @@ def execute_part1():
     from_down_shift = np.pad(from_down, (0, 1), constant_values=-1)[1:, :-1]
     from_right_shift = np.pad(from_right, (0, 1), constant_values=-1)[:-1, 1:]
     visibles = ((grid - from_top_shift) > 0) + ((grid - from_left_shift) > 0) + ((grid - from_down_shift) > 0) + (
-            (grid - from_right_shift) > 0)
+        (grid - from_right_shift) > 0)
     return np.sum(visibles)
 
 
 def execute_part2():
     with open(Path(dirname(__file__)) / f"input.txt", "r", encoding="utf-8") as f:
-        # with open(Path(dirname(__file__)) / f"test_input.txt", "r", encoding="utf-8") as f:
+    # with open(Path(dirname(__file__)) / f"test_input.txt", "r", encoding="utf-8") as f:
         data = f.read().split('\n')
+    grid = np.array([list(map(int, d)) for d in data])
+    #     I haven't found way to do it all using numpy
+    views = np.ones_like(grid)
+    for index, x in np.ndenumerate(grid):
+        from_top = grid[:index[0], index[1]]
+        from_left = grid[index[0], :index[1]]
+        from_down = grid[index[0] + 1:, index[1]]
+        from_right = grid[index[0], index[1] + 1:]
+        views[index] *= ind[0] + 1 if len(ind := np.where(x <= from_top[::-1])[0]) > 0 else from_top.size
+        views[index] *= ind[0] + 1 if len(ind := np.where(x <= from_left[::-1])[0]) > 0 else from_left.size
+        views[index] *= ind[0] + 1 if len(ind := np.where(x <= from_down)[0]) > 0 else from_down.size
+        views[index] *= ind[0] + 1 if len(ind := np.where(x <= from_right)[0]) > 0 else from_right.size
+    return np.max(views)
 
 
 if __name__ == '__main__':
@@ -37,7 +50,7 @@ if __name__ == '__main__':
     tac = time.perf_counter()
     res2 = execute_part2()
     toc = time.perf_counter()
-    submit_day(res1, 8, 1)
+    # submit_day(res1, 8, 1)
     # submit_day(res2, 8, 2)
     print(f"day 08 part 1 in {prettytime(tac - tic)}, answer: {res1}")
     print(f"day 08 part 2 in {prettytime(toc - tac)}, answer: {res2}")
