45225a65f4dd19679669be8d10869ee9b8f61983
Matěj Račinský <racinsky.matej@seznam.cz>
2022-12-10 17:05:57 +0100

    formatted

--- days/day_10/main.py
+++ days/day_10/main.py
@@ -11,6 +11,7 @@ def add_strength(cycle, register, tot_strength):
         return tot_strength + cycle * register
     return tot_strength
 
+
 def execute_instructions(instructions, register, perform_tick):
     for instruction in instructions:
         match instruction.split(' '):
@@ -22,22 +23,27 @@ def execute_instructions(instructions, register, perform_tick):
                 register += int(x)
     return register
 
+
 def execute_part1():
     input_file = "input.txt"
     # input_file = "test_input.txt"
     with open(Path(dirname(__file__)) / input_file, "r", encoding="utf-8") as f:
         data = f.read().split('\n')
     cycle, register, tot_strength = 0, 1, 0
+
     def perform_tick(register):
         nonlocal cycle, tot_strength
         cycle += 1
         tot_strength = add_strength(cycle, register, tot_strength)
         return register
+
     execute_instructions(data, register, perform_tick)
     return tot_strength
 
+
 def draw2grid(cycle, register, grid):
     grid[np.unravel_index(cycle, grid.shape)] = '#' if register - 1 <= (cycle % 40) <= register + 1 else '.'
+
 
 def execute_part2():
     input_file = "input.txt"
@@ -46,15 +52,18 @@ def execute_part2():
         data = f.read().split('\n')
     cycle, register, grid = 0, 1, np.chararray((6, 40), itemsize=1)
     grid[:] = ' '
+
     def perform_tick(register):
         nonlocal cycle, grid
         draw2grid(cycle, register, grid)
         cycle += 1
         return register
+
     execute_instructions(data, register, perform_tick)
     grid[grid == b'.'] = ' '
     np.apply_along_axis(lambda x: print(x.tostring()), axis=1, arr=grid)
     return 'PLGFKAZG'
+
 
 if __name__ == '__main__':
     read_day(10)
