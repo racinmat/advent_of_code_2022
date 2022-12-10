9b1216ff38cf44fa705b49b50a21ae92f4077c77
Matěj Račinský <racinsky.matej@seznam.cz>
2022-12-10 16:47:02 +0100

    refactored

--- days/day_10/main.py
+++ days/day_10/main.py
@@ -1,15 +1,21 @@
 import re
 import time
+from functools import partial
 from os.path import dirname
 from pathlib import Path
 import numpy as np
 from misc import read_day, submit_day, prettytime
 
 
-def add_strength(cycle, register):
+def count_strength(cycle, register):
     if (cycle - 20) % 40 == 0:
         return cycle * register
     return 0
+
+
+def tick(cycle: int, fun: callable):
+    cycle += 1
+    return cycle, fun(cycle)
 
 
 def execute_part1():
@@ -23,16 +29,20 @@ def execute_part1():
     for instruction in data:
         match instruction.split(' '):
             case ['noop']:
-                cycle += 1
-                tot_strength += add_strength(cycle, register)
+                cycle, strength = tick(cycle, partial(count_strength, register=register))
+                tot_strength += strength
             case ['addx', x] if re.match(r'-?\d+', x):
-                cycle += 1
-                tot_strength += add_strength(cycle, register)
-                cycle += 1
-                tot_strength += add_strength(cycle, register)
+                cycle, strength = tick(cycle, partial(count_strength, register=register))
+                tot_strength += strength
+                cycle, strength = tick(cycle, partial(count_strength, register=register))
+                tot_strength += strength
                 register += int(x)
         # print(f'{instruction=}, {cycle=}, {register=}, {tot_strength=}')
     return tot_strength
+
+
+def draw_grid(cycle, register, grid):
+    grid[np.unravel_index(cycle, grid.shape)] = '#' if register - 1 <= (cycle % 40) <= register + 1 else '.'
 
 
 def execute_part2():
@@ -40,30 +50,18 @@ def execute_part2():
     # input_file = "test_input.txt"
     with open(Path(dirname(__file__)) / input_file, "r", encoding="utf-8") as f:
         data = f.read().split('\n')
-    cycle = 0
+    cycle = -1
     register = 1
     grid = np.chararray((6, 40), itemsize=1)
     grid[:] = ' '
     for instruction in data:
         match instruction.split(' '):
             case ['noop']:
-                grid[np.unravel_index(cycle, grid.shape)] = '#' if register - 1 <= (cycle % 40) <= register + 1 else '.'
-                cycle += 1
-                a_row = cycle // 40
-                # print(grid.tostring()[a_row*40:(a_row+1)*40])
+                cycle, _ = tick(cycle, partial(draw_grid, register=register, grid=grid))
             case ['addx', x] if re.match(r'-?\d+', x):
-                grid[np.unravel_index(cycle, grid.shape)] = '#' if register - 1 <= (cycle % 40) <= register + 1 else '.'
-                cycle += 1
-                # print(grid)
-                a_row = cycle // 40
-                # print(grid.tostring()[a_row*40:(a_row+1)*40])
-                grid[np.unravel_index(cycle, grid.shape)] = '#' if register - 1 <= (cycle % 40) <= register + 1 else '.'
-                cycle += 1
-                # print(grid)
-                a_row = cycle // 40
-                # print(grid.tostring()[a_row*40:(a_row+1)*40])
+                cycle, _ = tick(cycle, partial(draw_grid, register=register, grid=grid))
+                cycle, _ = tick(cycle, partial(draw_grid, register=register, grid=grid))
                 register += int(x)
-        # print(f'{instruction=}, {cycle=}, {register=}, {tot_strength=}')
     grid[grid == b'.'] = ' '
     np.apply_along_axis(lambda x: print(x.tostring()), axis=1, arr=grid)
     return 'PLGFKAZG'
@@ -77,6 +75,16 @@ if __name__ == '__main__':
     res2 = execute_part2()
     toc = time.perf_counter()
     # submit_day(res1, 10, 1)
-    submit_day(res2, 10, 2)
+    # submit_day(res2, 10, 2)
     print(f"day 10 part 1 in {prettytime(tac - tic)}, answer: {res1}")
     print(f"day 10 part 2 in {prettytime(toc - tac)}, answer: {res2}")
+
+# b'###  #     ##  #### #  #  ##  ####  ##  '
+# b'#  # #    #  # #    # #  #  #    # #  # '
+# b'#  # #    #    ###  ##   #  #   #  #    '
+# b'###  #    # ## #    # #  ####  #   # ## '
+# b'#    #    #  # #    # #  #  # #    #  # '
+# b'#    ####  ### #    #  # #  # ####  ### '
+
+# day 10 part 1 in 763.600 μs, answer: 15880
+# day 10 part 2 in 1.855 ms, answer: PLGFKAZG
