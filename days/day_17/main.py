d4a48fa9c7247402de3c8696dca5cd77da6d69f7
Matěj Račinský <racinsky.matej@seznam.cz>
2022-12-20 17:26:45 +0100

    done d17 part 1

--- days/day_17/main.py
+++ days/day_17/main.py
@@ -6,8 +6,8 @@ import numpy as np
 
 
 def execute_part1():
-    # input_file = "input.txt"
-    input_file = "test_input.txt"
+    input_file = "input.txt"
+    # input_file = "test_input.txt"
     with open(Path(dirname(__file__)) / input_file, "r", encoding="utf-8") as f:
         pattern = f.read()
     shapes_txt = """\
@@ -37,8 +37,8 @@ def execute_part1():
             shape[i, [j == '#' for j in l]] = 1
         shapes.append(shape)
 
-    # grid = np.zeros((5_000, 7), dtype=np.int8)
-    grid = np.zeros((30, 7), dtype=np.int8)
+    grid = np.zeros((10_000, 7), dtype=np.int8)
+    # grid = np.zeros((30, 7), dtype=np.int8)
     depth, tot_width = grid.shape
     j = -1
     for i in range(2022):
@@ -49,33 +49,51 @@ def execute_part1():
             y = depth - 3
         else:
             y = np.argmax(np.max(grid, axis=1), axis=0) - 3
-        print(f'starts falling: {x=}, {y=}')
+        print(f'{i}th starts falling: {x=},{y=}')
         even = False
+        hit_floor = False
         while True:
             copy_grid = np.copy(grid)
             old_x, old_y = x, y
             if even:
                 y += 1
-                print(f'falls down')
+                print(f'falls down to {x=},{y=}')
+                if y > depth:
+                    hit_floor = True
+                    break
             else:
                 j += 1
                 j %= len(pattern)
                 p = pattern[j]
                 if p == '>':
-                    # todo: add here the test for not being pushed towards stone
                     x = min(x + 1, tot_width - width)
+                    copy_grid[y - height:y, x:x + width] += shape
+                    if np.any(copy_grid > 1):
+                        # not moving
+                        x = old_x
+                    copy_grid = np.copy(grid)
                     print(f'pushes right{", nothing happens" if x == tot_width - width else ""}, {x=}, {y=}')
                 else:
                     x = max(x - 1, 0)
+                    copy_grid[y - height:y, x:x + width] += shape
+                    if np.any(copy_grid > 1):
+                        # not moving
+                        x = old_x
+                    copy_grid = np.copy(grid)
                     print(f'pushes left{", nothing happens" if x == 0 else ""}, {x=}, {y=}')
-            copy_grid[y - height:y, x:x + width] += shape
             even = not even
-            # todo: if sth should be pushed towards other stone but can fall down, I should continue and now push it
-            if np.any(copy_grid > 1) or y > depth:
+            # wtf, něco se děje, asi bych měl mít y == depth?
+            if hit_floor:
                 break
+            else:
+                copy_grid[y - height:y, x:x + width] += shape
+                if np.any(copy_grid > 1):
+                    break
         grid[old_y - height:old_y, old_x:old_x + width] += shape
 
-    print('')
+    return int(depth - np.argmax(np.max(grid, axis=1), axis=0))
+
+
 def execute_part2():
     # input_file = "input.txt"
     input_file = "test_input.txt"
@@ -90,7 +108,7 @@ if __name__ == '__main__':
     tac = time.perf_counter()
     res2 = execute_part2()
     toc = time.perf_counter()
-    # submit_day(res1, 17, 1)
+    submit_day(res1, 17, 1)
     # submit_day(res2, 17, 2)
     print(f"day 17 part 1 in {prettytime(tac - tic)}, answer: {res1}")
     print(f"day 17 part 2 in {prettytime(toc - tac)}, answer: {res2}")
