ef99b6b90215243bd57be258864e6919e69f65a2
Matěj Račinský <racinsky.matej@seznam.cz>
2022-12-22 00:01:06 +0100

    prototyped first iter of day 20

--- days/day_20/main.py
+++ days/day_20/main.py
@@ -1,26 +1,36 @@
+import copy
 import time
 from os.path import dirname
 from pathlib import Path
-import psycopg2
+import numpy as np
 from misc import read_day, submit_day, prettytime
 
 
 def execute_part1():
-    conn = psycopg2.connect(f"dbname=postgres user=postgres password=example")
-    with conn:
-        with conn.cursor() as cursor:
-            with open(Path(dirname(__file__)) / f"part1.sql", "r", encoding="utf-8") as f:
-                cursor.execute(f.read())
-                return cursor.fetchone()[0]
-
+    # input_file = "input.txt"
+    input_file = "test_input.txt"
+    with open(Path(dirname(__file__)) / input_file, "r", encoding="utf-8") as f:
+        numbers = [int(i) for i in f.read().split('\n')]
+    orig_numbers = copy.copy(numbers)
+    orig_indices = list(range(len(orig_numbers)))
+    cur_indices = copy.copy(orig_indices)
+    print(f'{", ".join(map(str, numbers))}')
+    for i in orig_indices:
+        val = orig_numbers[i]
+        # cur_idx = cur_indices[i]
+        # I need inverse search, not direct
+        cur_idx = next(filter(lambda xy: xy[1] == i, enumerate(cur_indices)))[0]
+        del numbers[cur_idx]
+        numbers.insert(cur_idx + val, val)
+        del cur_indices[cur_idx]
+        cur_indices.insert(cur_idx + val, i)
+        print(f'{", ".join(map(str, numbers))}')
 
 def execute_part2():
-    conn = psycopg2.connect(f"dbname=postgres user=postgres password=example")
-    with conn:
-        with conn.cursor() as cursor:
-            with open(Path(dirname(__file__)) / f"part2.sql", "r", encoding="utf-8") as f:
-                cursor.execute(f.read())
-                return cursor.fetchone()[0]
+    # input_file = "input.txt"
+    input_file = "test_input.txt"
+    with open(Path(dirname(__file__)) / input_file, "r", encoding="utf-8") as f:
+        numbers = [int(i) for i in f.read().split('\n')]
 
 
 if __name__ == '__main__':
@@ -30,7 +40,7 @@ if __name__ == '__main__':
     tac = time.perf_counter()
     res2 = execute_part2()
     toc = time.perf_counter()
-    submit_day(res1, 20, 1)
-    submit_day(res2, 20, 2)
+    # submit_day(res1, 20, 1)
+    # submit_day(res2, 20, 2)
     print(f"day 20 part 1 in {prettytime(tac - tic)}, answer: {res1}")
     print(f"day 20 part 2 in {prettytime(toc - tac)}, answer: {res2}")
--- days/day_20/part1.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS dec20;
-
-CREATE UNLOGGED TABLE dec20 (
-    line_number bigint NOT NULL GENERATED ALWAYS AS IDENTITY,
-    row_data text NOT NULL
-);
-
-COPY dec20 (row_data) FROM '/aoc/days/day_20/input.txt';
-VACUUM ANALYZE dec20;
